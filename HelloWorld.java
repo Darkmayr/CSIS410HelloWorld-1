0ddc0aadea5677cddc043ffdbf8bead2d885a5be
London <london.brunell@gmail.com>
2017-10-10 15:56:41 -0400

    Added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@
 /**
  * Write a description of class HelloWorld here.
  *
+ * 10/10/2017 London Brunell, Clarice Tarbay
+ *
  * @author Brian Smith and Hamza Memon
  * @version (a version number or a date)
  */
